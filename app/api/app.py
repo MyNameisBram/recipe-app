7683885e2db828465e88b4dd6d2367d67d64d07a
MyNameisBram <42178252+MyNameisBram@users.noreply.github.com>
2023-05-02 14:25:31 -0400

    Update app.py

--- app/api/app.py
+++ app/api/app.py
@@ -1,1 +1,18 @@
+from flask import Flask, request, jsonify
+import requests
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+
+@app.route('/recipe', methods=['POST'])
+def get_recipe():
+    url = request.json['url']
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    instructions = soup.find('div', {'class': 'instructions'}).find('p').text
+
+    return jsonify({'instructions': instructions})
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
